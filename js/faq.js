c0c296001da35ee02859bebd75418e0e76db3422
Paulo-Estalise <phestalise@gmail.com>
2025-05-20 00:32:49 -0300

    selecionando  todos os elementos de perguntas do FAQ e  Criando a função que alterna a resposta ao clicar

--- js/faq.js
+++ js/faq.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const perguntasFAQ = document.querySelectorAll('.faq-question');
+     function alternarResposta(event) {
+        const perguntaClicada = event.currentTarget;
 
-    
+      
+        perguntasFAQ.forEach(pergunta => {
+            if (pergunta !== perguntaClicada) {
+                pergunta.classList.remove('active');
+                pergunta.nextElementSibling.classList.remove('show');
+            }
+        });
+
+      
+        perguntaClicada.classList.toggle('active');
+        const resposta = perguntaClicada.nextElementSibling;
+        resposta.classList.toggle('show');
+
+        console.log("Pergunta clicada: ", perguntaClicada.textContent);
+    }
 
 
 
